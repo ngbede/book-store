3b424d85f4b68f1ec23cb6a3ed05772ec0035597
Emmanuel Sule <ogasule001@gmail.com>
2022-04-15 19:07:06 +0100

    disable cors

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const bookRoute = require("./routes/book-routes")
 const app = express()
 
 app.use(express.json()) // Json middleware
+
+// Disable CORS
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    next()
+})
+
 app.use("", bookRoute)
 
 const PORT = process.env.PORT || 3000
